5b7358b413420695a0991d6977c5a59de14b39ba
KAUSHIK LAHIRI <lkaushik@in.ibm.com>
2020-04-20 17:09:44 +0530

    Standard DB schema across Python/Springboot

--- routes/request_api.py
+++ routes/request_api.py
@@ -1,28 +1,32 @@
 """The Endpoints to manage the Health record requests"""
 
-import uuid
-from flask import jsonify, abort, request, Blueprint
+import copy
+from flask import json, jsonify, abort, request, Blueprint
 from validate_email import validate_email
+from bson import json_util
+
 # import the MongoClient classes
 from pymongo import MongoClient, errors
 
 REQUEST_API = Blueprint('request_api', __name__)
 
+# read property file for attributes
 propertydict = {}
 with open("static/properties.txt") as pfile:
     for line in pfile:
         key, value = line.partition("=")[::2]
         propertydict[key] = value.strip()
 properties = type("Names", (object,), propertydict)
-print("mongodburl:", properties.mongodburl)
-print("mongodbname:", properties.mongodbname)
+print("MongoDB URL:", properties.mongodburl)
+print("MongoDB database name:", properties.mongodbname)
 
+# instantiate MongoDb connection and database
 client = MongoClient(properties.mongodburl)
 db = client.get_database(properties.mongodbname)
+patientrecords = db.patients
 
 # print the version of MongoDB server if connection successful
 print ("MongoDB server version:", client.server_info()["version"])
-
 
 def get_blueprint():
     """Return the blueprint for the main app module"""
@@ -30,143 +34,57 @@ def get_blueprint():
 
 @REQUEST_API.route('/request', methods=['GET'])
 def get_records():
-    PATIENT_LIST = {}
     # Retrieve all patient records
-    patients = db.patients.find()
+    patients = patientrecords.find()
+    PATIENT_LIST = []
     for patient in patients:
-        PATIENT_LIST[patient['id']] = {
-            'fullname': patient['fullname'],
-            'age': patient['age'],
-            'gender': patient['gender'],
-            'height': patient['height'],
-            'weight': patient['weight'],
-            'maritalStatus': patient['maritalStatus'],
-            'allergies': patient['allergies'],
-            'tobaccoUse': patient['tobaccoUse'],
-            'email': patient['email']}
+        del patient['_id'] # Object id is not JSON serializable
+        PATIENT_LIST.append(patient)
     return jsonify(PATIENT_LIST)
 
 @REQUEST_API.route('/request/<string:_id>', methods=['GET'])
 def get_record_by_id(_id):
-    PATIENT_LIST = {}
-    # Retrieve all patient records to find the one with the given key
-    patients = db.patients.find()
-    for patient in patients:
-        PATIENT_LIST[patient['id']] = {
-            'fullname': patient['fullname'],
-            'age': patient['age'],
-            'gender': patient['gender'],
-            'height': patient['height'],
-            'weight': patient['weight'],
-            'maritalStatus': patient['maritalStatus'],
-            'allergies': patient['allergies'],
-            'tobaccoUse': patient['tobaccoUse'],
-            'email': patient['email']}
-    if _id in PATIENT_LIST.keys():
-        return jsonify(PATIENT_LIST[_id])
-    else:
+    patient = patientrecords.find_one({"id":_id})
+    if not patient:
         abort(404)
     # Return with a 404 error if id does not exist
+    del patient['_id'] # Object id is not JSON serializable
+    foundPatient = copy.deepcopy(patient)
+    return jsonify(foundPatient)
 
 @REQUEST_API.route('/request', methods=['POST'])
 def create_record():
     if not request.get_json():
-        abort(400)
+        abort(400, description="Request has incorrect JSON format")
     data = request.get_json(force=True)
-
-    if not data.get('fullname'):
-        abort(400)
-    if not validate_email(data['email']):
-        abort(400)
-    if not data.get('age'):
-        abort(400)
-
-    # Generate a new key, insert a record against this key and return the key
-    new_uuid = str(uuid.uuid4())
-    db.patients.insert_one(
-       {
-            'id':new_uuid,
-            'fullname': data.get('fullname'),
-            'age': data.get('age'),
-            'gender': data.get('gender'),
-            'height': data.get('height'),
-            'weight': data.get('weight'),
-            'maritalStatus': data.get('maritalStatus'),
-            'allergies': data.get('allergies'),
-            'tobaccoUse': data.get('tobaccoUse'),
-            'email': data['email']
-        }
-    )
+    if not data.get('id'):
+        abort(400, description="Request does not have id")
+    if not validate_email(data['mailId']):
+        abort(400, description="Request email has incorrect email format")
+    newPatient = copy.deepcopy(data)
+    patientrecords.insert_one(newPatient)
     # HTTP 201 Created
-    return jsonify({"id": new_uuid}), 201
+    return jsonify({"id": data.get('id')}), 201
 
 @REQUEST_API.route('/request/<string:_id>', methods=['PUT'])
 def edit_record(_id):
-    PATIENT_LIST = {}
-    patients = db.patients.find()
-    # Search through all patient records for key, return 404 error if not found
-    for patient in patients:
-        PATIENT_LIST[patient['id']] = {
-            'fullname': patient['fullname'],
-            'age': patient['age'],
-            'gender': patient['gender'],
-            'height': patient['height'],
-            'weight': patient['weight'],
-            'maritalStatus': patient['maritalStatus'],
-            'allergies': patient['allergies'],
-            'tobaccoUse': patient['tobaccoUse'],
-            'email': patient['email']}
-
-    if _id not in PATIENT_LIST.keys():
-        abort(404)
     if not request.get_json():
         abort(400)
     data = request.get_json(force=True)
-
-    if not data.get('fullname'):
+    if not data['id']:
         abort(400)
-    if not validate_email(data['email']):
+    if not validate_email(data['mailId']):
         abort(400)
-    if not data.get('age'):
-        abort(400)
-
-    # replace the record having the given key with the new values from request
-    db.patients.replace_one(
-        {'id':_id},
-        {
-            'id':_id,
-            'fullname': data.get('fullname'),
-            'age': data.get('age'),
-            'gender': data.get('gender'),
-            'height': data.get('height'),
-            'weight': data.get('weight'),
-            'maritalStatus': data.get('maritalStatus'),
-            'allergies': data.get('allergies'),
-            'tobaccoUse': data.get('tobaccoUse'),
-            'email': data['email']
-        }
-    )
-
-    return jsonify(PATIENT_LIST[_id]), 200
-
+    if not patientrecords.find_one({"id":_id}):
+        abort(404)
+    updatedPatient = copy.deepcopy(data)
+    patientrecords.replace_one({'id':_id}, updatedPatient)
+    # HTTP 201 Created
+    return jsonify(updatedPatient), 201
 
 @REQUEST_API.route('/request/<string:_id>', methods=['DELETE'])
 def delete_record(_id):
-    PATIENT_LIST = {}
-    patients = db.patients.find()
-    for patient in patients:
-        PATIENT_LIST[patient['id']] = {
-            'fullname': patient['fullname'],
-            'age': patient['age'],
-            'gender': patient['gender'],
-            'height': patient['height'],
-            'weight': patient['weight'],
-            'maritalStatus': patient['maritalStatus'],
-            'allergies': patient['allergies'],
-            'tobaccoUse': patient['tobaccoUse'],
-            'email': patient['email']}
-
-    if _id not in PATIENT_LIST.keys():
+    if not patientrecords.find_one({"id":_id}):
         abort(404)
-    db.patients.delete_one({"id":_id})
+    patientrecords.delete_one({'id':_id})
     return '', 204
